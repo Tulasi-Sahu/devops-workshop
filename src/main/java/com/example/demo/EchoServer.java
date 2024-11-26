9ae47fe1a0ae2c62d65f723d22db9dfab6e43ce8
Tulasi-Sahu <tulasisahu2004@gmail.com>
2024-11-26 12:01:58 +0530

    fixed bug

--- src/main/java/com/example/demo/EchoServer.java
+++ src/main/java/com/example/demo/EchoServer.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class EchoServer {
     @GetMapping("/echo/{name}")
     public String echo(@PathVariable("name")String name){
-        return "Hello " + name;
+        return "Hello " + name + "welcome to devops workshop\n";
     }
 }
